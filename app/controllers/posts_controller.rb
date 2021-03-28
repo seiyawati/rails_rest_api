0f922133757e963f835381c0f5882c2ff0cae96a
Seiya Kawamoto <kid960805@gmail.com>
2021-03-29 01:52:57 +0900

    fix: posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
 
   def index
-    posts = ::Post.order(created_at: :desc)
+    posts = Post.order(created_at: :desc)
     render json: { status: 'SUCCESS', message: 'Loaded posts', data: posts }
   end
 
